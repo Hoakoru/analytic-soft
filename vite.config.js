22bb8f75bd01555a35c92afac991413d9de48b94
Hoakoru <jorgejoaquinmv@gmail.com>
2023-09-01 21:11:37 -0400

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://hoakoru.github.io/project3/"
+  plugins: [react()]
 })
