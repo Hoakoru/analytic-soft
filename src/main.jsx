02881798beed101f860d19f001d331e96390e742
Hoakoru <jorgejoaquinmv@gmail.com>
2023-09-13 04:31:56 -0400

    update config

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/analytic-soft/">
+    <BrowserRouter /* basename="/analytic-soft/" */>
       <App />
     </BrowserRouter>
   </React.StrictMode>
